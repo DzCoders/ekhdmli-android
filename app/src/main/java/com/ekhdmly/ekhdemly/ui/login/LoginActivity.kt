f1b4fa658d023481d9db2eccbcc555471d0f9cf6
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-15 16:19:43 +0100

    First Commit, Packages setup and login UI impl

---
+++ app/src/main/java/com/ekhdmly/ekhdemly/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.ekhdmly.ekhdemly.ui.login
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import me.ekhdmly.ekhdemly.R
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
