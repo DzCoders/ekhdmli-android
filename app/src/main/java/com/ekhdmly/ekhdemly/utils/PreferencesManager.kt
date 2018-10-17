f2cacf22523a0296d6ce5014231768e56597bc2b
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-17 09:42:26 +0100

    Adding Hash functionality to The Shared Prefrences File

--- app/src/main/java/com/ekhdmly/ekhdemly/utils/PreferencesManager.kt
+++ app/src/main/java/com/ekhdmly/ekhdemly/utils/PreferencesManager.kt
@@ -13,6 +13,7 @@ class PreferencesManager(context: Context) {
         const val FIRST_TIME = "isFirstTime"
         const val LOGIN = "Login"
         const val USER_ID = "userId"
+        const val HASH = "hash"
     }
 
     private val preference: SharedPreferences
@@ -24,26 +25,48 @@ class PreferencesManager(context: Context) {
         editor = preference.edit()
     }
 
-    // Used to track the App State if First Or Nor
+    // Used to track the App State if First Or Not.
     var isFirstTime: Boolean
         get() = !preference.getBoolean(FIRST_TIME, true)
         set(value) {
             editor.putBoolean(FIRST_TIME, false).commit()
         }
 
-    // Used to track the User State of Login or NOt
+    // Used to track the User State of Login or Not.
     var isLogin: Boolean
         get() = preference.getBoolean(LOGIN, false)
         set(value) {
             editor.putBoolean(LOGIN, value).commit()
         }
 
-    // Used to track the User ID
+    // Used to track the User ID.
     var userId: Long
         get() = preference.getLong(USER_ID, -1)
         set(value) {
             editor.putLong(USER_ID, value).commit()
         }
+
+    // Hash of the File.
+    var hash : String?
+        get() = preference.getString(HASH,null)
+        set(value){
+            editor.putString(HASH , value)
+        }
+
+    /**
+     * This Function calculate the current Hash of file.
+     */
+    fun calculateCurrentHash() : String{
+        return ""
+    }
+
+    /**
+     * This Function calculate the Hash of a given data.
+     * @param data : the data array we want to hash.
+     */
+    fun calculateHash(data : Array<String>) : String {
+        return ""
+    }
 
     fun removePreference() = editor.clear().commit()
 }
